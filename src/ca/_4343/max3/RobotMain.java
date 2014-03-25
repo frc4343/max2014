ece823bb6b78a59ac0b54451f55000d53a43601b
TediPapajorgji <tedi.papajorgji@hotmail.com>
2014-03-24 23:21:19 -0400

    Fixed autonomous instantiation stuff
    No support for conditional command groups, had to figure a way around
    it. Basically make it run on autonomous init

--- src/ca/_4343/max3/RobotMain.java
+++ src/ca/_4343/max3/RobotMain.java
@@ -8,6 +8,7 @@
 package ca._4343.max3;
 
 
+import ca._4343.max3.commandGroups.autonomous.SelectAutonomous;
 import ca._4343.max3.commandGroups.autonomous.hot.DoubleBallAlternativeLeftSequence;
 import ca._4343.max3.commandGroups.autonomous.hot.DoubleBallAlternativeRightSequence;
 import edu.wpi.first.wpilibj.IterativeRobot;
@@ -21,6 +22,7 @@ import ca._4343.max3.commandGroups.autonomous.ignoreHot.DoubleBallNotHotSequence
 import ca._4343.util.Camera;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -39,6 +41,7 @@ public class RobotMain extends IterativeRobot {
      * used for any initialization code.
      */
     public void robotInit() {
+        initializeVirtualButtons();
         // instantiate the command used for the autonomous period
         //autonomousCommand = new ExampleCommand();
 
@@ -46,19 +49,44 @@ public class RobotMain extends IterativeRobot {
         CommandBase.init();
         //Camera.init();
         
-        pickAutonomous = new SendableChooser();
-        pickAutonomous.addDefault("1 Ball Hot | Any Side", new SingleBallSequence());
-        pickAutonomous.addObject("2 Ball No Hot | Any Side", new DoubleBallNotHotSequence());
-        pickAutonomous.addObject("2 Ball Hot | Left Side", new DoubleBallLeftSequence());
-        pickAutonomous.addObject("2 Ball Hot | Right Side", new DoubleBallRightSequence());
-        pickAutonomous.addObject("2 Ball Hot ALT | Left Side", new DoubleBallAlternativeLeftSequence());
-        pickAutonomous.addObject("2 Ball Hot ALT | Right Side", new DoubleBallAlternativeRightSequence());
+        
         
     }
 
     public void autonomousInit() {
-        autonomousCommandGroup = (CommandGroup) pickAutonomous.getSelected();
-        autonomousCommandGroup.start();
+        autonomousCommandGroup = getSelectedCommandGroup((SelectAutonomous) pickAutonomous.getSelected());
+        if(autonomousCommandGroup != null)
+            autonomousCommandGroup.start();
+    }
+    
+    private CommandGroup getSelectedCommandGroup(SelectAutonomous sa) {
+        /* Java 1.3 is so bad...........
+    * These are possible values for type:
+    * 1 - 1 Ball Hot | Any Side
+    * 2 - 2 Ball No Hot | Any Side
+    * 3 - 2 Ball Hot | Left Side
+    * 4 - 2 Ball Hot | Right Side
+    * 5 - 2 Ball Hot ALT | Left Side
+    * 6 - 2 Ball Hot ALT | Right Side
+    */
+        if(sa != null) {
+            int autonomousType = Integer.parseInt(sa.toString());
+            switch(autonomousType) {
+                case 1:
+                    return new SingleBallSequence();
+                case 2:
+                    return new DoubleBallNotHotSequence();
+                case 3:
+                    return new DoubleBallLeftSequence();
+                case 4:
+                    return new DoubleBallRightSequence();
+                case 5:
+                    return new DoubleBallAlternativeLeftSequence();
+                case 6:
+                    return new DoubleBallAlternativeRightSequence();
+            }
+        }
+        return null;
     }
 
     /**
@@ -89,4 +117,22 @@ public class RobotMain extends IterativeRobot {
     public void testPeriodic() {
         LiveWindow.run();
     }
+    
+    private void initializeVirtualButtons() {
+        pickAutonomous = new SendableChooser();
+        /*pickAutonomous.addDefault("1 Ball Hot | Any Side", new SingleBallSequence());
+        pickAutonomous.addObject("2 Ball No Hot | Any Side", new DoubleBallNotHotSequence());
+        pickAutonomous.addObject("2 Ball Hot | Left Side", new DoubleBallLeftSequence());
+        pickAutonomous.addObject("2 Ball Hot | Right Side", new DoubleBallRightSequence());
+        pickAutonomous.addObject("2 Ball Hot ALT | Left Side", new DoubleBallAlternativeLeftSequence());
+        pickAutonomous.addObject("2 Ball Hot ALT | Right Side", new DoubleBallAlternativeRightSequence());*/
+        pickAutonomous.addDefault("1 Ball Hot | Any Side", new SelectAutonomous(1));
+        pickAutonomous.addObject("2 Ball No Hot | Any Side", new SelectAutonomous(2));
+        pickAutonomous.addObject("2 Ball Hot | Left Side", new SelectAutonomous(3));
+        pickAutonomous.addObject("2 Ball Hot | Right Side", new SelectAutonomous(4));
+        pickAutonomous.addObject("2 Ball Hot ALT | Left Side", new SelectAutonomous(5));
+        pickAutonomous.addObject("2 Ball Hot ALT | Right Side", new SelectAutonomous(6));
+        SmartDashboard.putData("Select Autonomous Mode", pickAutonomous);
+    }
+    
 }
---
+++ src/ca/_4343/max3/commandGroups/autonomous/SelectAutonomous.java
@@ -0,0 +1,33 @@
+/*
+ * Team 4343
+ * Visit us at www.4343.ca
+ */
+
+package ca._4343.max3.commandGroups.autonomous;
+
+/**
+ *
+ * @author Tedi Papajorgji <www.4343.ca>
+ */
+public class SelectAutonomous {
+    /* Java 1.3 is so bad...........
+    * These are possible values for type:
+    * 1 - 1 Ball Hot | Any Side
+    * 2 - 2 Ball No Hot | Any Side
+    * 3 - 2 Ball Hot | Left Side
+    * 4 - 2 Ball Hot | Right Side
+    * 5 - 2 Ball Hot ALT | Left Side
+    * 6 - 2 Ball Hot ALT | Right Side
+    */
+    
+    public SelectAutonomous(int type) {
+        nameOfAutonomous = "" + type;
+    }
+    
+    private final String nameOfAutonomous;
+    
+    public String toString() {
+        return nameOfAutonomous;
+    }
+    
+}
